2989cb5303d56daadc568968dd6e2a60ea60ec5c
Orlon <Orlon@Aspee-HP>
2015-12-29 23:53:50 +0530

    Look and Feel

--- src/crms/Login.java
+++ src/crms/Login.java
@@ -11,8 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.sql.ResultSet;
-import java.util.Arrays;
+import javax.swing.JLabel;
 
 /**
  *
@@ -28,11 +27,14 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
     int tmp;
 
     public Login() {
+	setContentPane(new JLabel(new javax.swing.ImageIcon(getClass().getResource("/res/background.jpg"))));
         initComponents();
+        setSize(534,243);
+        tfUsername.requestFocus(); 
         setResizable(false);
         setLocationRelativeTo(null);
         database = new Database();
-        lCaps.setForeground(Color.BLUE);
+        //lCaps.setForeground(Color.BLUE);
         lCaps.setText("Caps Lock is " + (Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK) ? "On" : "Off"));
 
         tfUsername.addKeyListener(Login.this);
@@ -62,20 +64,26 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         lPassword = new javax.swing.JLabel();
         tfPassword = new javax.swing.JPasswordField();
         lValid = new javax.swing.JLabel();
+        bLogin1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
+        setUndecorated(true);
 
         lLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/insignia.alpha.png"))); // NOI18N
         lLogo.setToolTipText("");
 
-        lCaps.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        lCaps.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
+        lCaps.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        lCaps.setForeground(new java.awt.Color(255, 255, 255));
+        lCaps.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel1.setOpaque(false);
 
-        bLogin.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        bLogin.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        bLogin.setForeground(new java.awt.Color(77, 112, 180));
         bLogin.setText("Login");
         bLogin.setAutoscrolls(true);
         bLogin.setOpaque(false);
@@ -93,10 +101,15 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
             }
         });
 
-        lUsername.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lUsername.setBackground(new java.awt.Color(255, 255, 255));
+        lUsername.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        lUsername.setForeground(new java.awt.Color(255, 255, 255));
+        lUsername.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
         lUsername.setText("Username:");
 
-        lPassword.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lPassword.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        lPassword.setForeground(new java.awt.Color(255, 255, 255));
+        lPassword.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
         lPassword.setText("Password:");
 
         tfPassword.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
@@ -107,7 +120,9 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
             }
         });
 
-        lValid.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lValid.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        lValid.setForeground(new java.awt.Color(255, 255, 255));
+        lValid.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
         lValid.setText(" ");
         lValid.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
@@ -116,7 +131,7 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(8, 8, 8)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -127,8 +142,8 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
                             .addComponent(tfUsername, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(tfPassword, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(lValid, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lValid, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(bLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
@@ -138,7 +153,7 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(18, 18, 18)
+                .addGap(15, 15, 15)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lUsername)
                     .addComponent(tfUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -156,32 +171,49 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         lValid.getAccessibleContext().setAccessibleName("jLabel4");
         lValid.getAccessibleContext().setAccessibleDescription("");
 
+        bLogin1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        bLogin1.setForeground(new java.awt.Color(77, 112, 180));
+        bLogin1.setText("Exit");
+        bLogin1.setAutoscrolls(true);
+        bLogin1.setOpaque(false);
+        bLogin1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bLogin1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lLogo)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(16, 16, 16)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lCaps, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(30, 30, 30))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lCaps, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(30, 30, 30))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(bLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(bLogin1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lCaps, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -199,6 +231,11 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         // TODO add your handling code here:
     }//GEN-LAST:event_tfPasswordActionPerformed
 
+    private void bLogin1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLogin1ActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_bLogin1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -208,7 +245,10 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        
         try {
+            javax.swing.UIManager.put("control", new Color(0xbed3e6));
+            javax.swing.UIManager.put("nimbusBase", new Color(0x878c96));
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -237,6 +277,7 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bLogin;
+    private javax.swing.JButton bLogin1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lCaps;
     private javax.swing.JLabel lLogo;
@@ -260,7 +301,7 @@ public class Login extends javax.swing.JFrame implements ActionListener, KeyList
 
         } else {
             lValid.setForeground(Color.red);
-            lValid.setText("Invalid Username or Password.");
+            lValid.setText("Invalid Credentials");
             java.awt.Toolkit.getDefaultToolkit().beep();
             tfUsername.setText("");
             tfPassword.setText("");
